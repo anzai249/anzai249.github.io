726c83b653f709d9aff80fb345130449e2747256
an <a381811169@163.com>
2023-07-20 21:02:34 +0800

    Site updated: 2023-07-20 21:02:17

---
+++ js/layouts/lazyload.js
@@ -0,0 +1,1 @@
+Global.initLazyLoad=(()=>{const t=document.querySelectorAll("img"),e=new IntersectionObserver((t,e)=>{t.forEach(t=>{if(t.isIntersecting){const r=t.target,o=r.getAttribute("data-src");r.src=o,r.removeAttribute("lazyload"),e.unobserve(r)}})},{rootMargin:"0px",threshold:.1});t.forEach(t=>{t.hasAttribute("lazyload")&&e.observe(t)})});
